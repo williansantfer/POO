e45b2be304da889632bbd1054ba0499dc58927b7
Willian Ferreira dos Santos <willsantfer@gmail.com>
2024-08-02 22:15:44 -0300

    aula-genericos

---
+++ aula-genericos/app/src/main/java/ads/poo/Caixa.java
@@ -0,0 +1,14 @@
+package ads.poo;
+
+public class Caixa<T> {
+
+    private T conteudo;
+
+    public T getConteudo() {
+        return conteudo;
+    }
+
+    public void setConteudo(T conteudo) {
+        this.conteudo = conteudo;
+    }
+}
---
+++ aula-genericos/app/src/main/java/ads/poo/Pessoa.java
@@ -0,0 +1,10 @@
+package ads.poo;
+
+public class Pessoa {
+
+    private String nome;
+
+    public Pessoa(String nome) {
+        this.nome = nome;
+    }
+}
